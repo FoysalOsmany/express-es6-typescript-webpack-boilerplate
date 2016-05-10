606dcb424152b7382deb2a5c3eb98eeb4597e905
FoysalOsmany <foysalo@gmail.com>
2016-05-10 17:14:32 +0600

    Typings for packages

---
+++ typings/browser.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="browser/ambient/body-parser/index.d.ts" />
+/// <reference path="browser/ambient/compression/index.d.ts" />
+/// <reference path="browser/ambient/express/index.d.ts" />
+/// <reference path="browser/ambient/helmet/index.d.ts" />
+/// <reference path="browser/ambient/mime/index.d.ts" />
+/// <reference path="browser/ambient/node/index.d.ts" />
---
+++ typings/main.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="main/ambient/body-parser/index.d.ts" />
+/// <reference path="main/ambient/compression/index.d.ts" />
+/// <reference path="main/ambient/express/index.d.ts" />
+/// <reference path="main/ambient/helmet/index.d.ts" />
+/// <reference path="main/ambient/mime/index.d.ts" />
+/// <reference path="main/ambient/node/index.d.ts" />
